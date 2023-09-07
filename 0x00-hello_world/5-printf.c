2f62158e0c58428283f2072e66e0fd2dcc1b2a1c
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-07 17:07:42 +0200

    completed 6 using sizeof()

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-printf("%c","with proper grammar, but the outcome is a piece of art,");
+#include <stdio.h>
+/**
+	* main - Entry point
+	* Description: Uses printf to print a char string
+	* Return: Always 0 (Success)
+	*/
+int main(void)
+{
+	char stringex2[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(stringex2)
+	return (0);
+}
+
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,14 @@
-#!/bin/bash
-sizeof(char);
-sizeof(int);
-sizeof();
-sizeof();
-sizeof(float)
+#include <stddef.h>
+/**
+	* Main - Entry point
+	* Description: This function returns the size of various data types in bytes
+	* Return: Always 0 (Success)
+	*/
+int main(void)
+{
+	printf("Size of a char: %d byte\(s\)\n", sizeof(char);
+	printf("Size of an int: %d byte\(s\)\n", sizeof(int));
+	printf("Size of a long int: %d byte\(s\)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte\(s\)\n", sizeof(double));
+	printf("Size of a float: %d byte\(s\)\n", sizeof(float));
+}
