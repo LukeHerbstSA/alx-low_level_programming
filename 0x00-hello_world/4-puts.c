096b42fef8f8524af2f88e7b2aa075677032c7cb
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-07 16:33:20 +0200

    completed c puts program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,17 @@
 !# / bin / bash
-# < stdio.h >
+#include< stdio.h >
+
+/*
+ * main - Entry point
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	char stringex = "\"Programming is like building a multilingual puzzle";
+	char stringex[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(stringex);
+	return (0);
 }
-main()
