682195c3532ad7c62e080dbb9994a66e0e35af3d
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-08 17:57:50 +0200

    finished betty - pretty easy

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+	* main - Entry point
+	* Description: Uses printf to print a char string
+	* Return: Always 0 (Success)
+	*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	remainder = n % 10
+
+	if (remainder > 5)
+		printf("Last digit of %d is %d  and is greater than 5", n, remainder);
+	else if (remainder < 6 && remainder != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, remainder);
+	else
+		printf("Last digit of %d is 0");
+	return (0);
+}
