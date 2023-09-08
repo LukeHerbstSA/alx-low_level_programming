723a244aa986a9a3a175cfbf17283b2b5352b5f4
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-08 19:20:47 +0200

    Finito

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+	* main - Entry point
+	*
+	* Return: Always 0 (Success)
+	*/
+int main(void)
+{
+	char chararray;
+
+	for (chararray = 'a'; chararray <= 'z'; chararray++)
+		putchar(chararray);
+	for (chararray = 'A', chararray <= 'Z', chararray++)
+		putchar(chararray)
+	putchar('\n')
+}
