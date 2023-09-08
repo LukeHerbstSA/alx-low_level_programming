8c0848c7660b7a1d15f48472292448e1670e3f8e
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-08 22:38:38 +0200

    fixed spacing mistake

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
-        * main - Entry point
-        * Description: Uses putchar to print a string
-        * Return: Always 0 (Success)
-        */
+	* main - Entry point
+	* Description: Uses putchar to print a string
+	* Return: Always 0 (Success)
+	*/
 int main(void)
 {
-        int num;
+	int num;
 
-        for (num = 0; num <= 9; num++)
-                putchar('0' + num);
-        putchar('\n');
-        return (0);
+	for (num = 0; num <= 9; num++)
+		putchar('0' + num);
+	putchar('\n');
+	return (0);
 }
