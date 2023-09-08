50aef82cb5664a064cb90d4870ee9211a4e572bc
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-08 18:10:23 +0200

    changed declaraction to expression as additional declarations are not allowed after a statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,13 +12,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int remainder = n % 10;
 
-	if (remainder > 5)
-		printf("Last digit of %d is %d  and is greater than 5", n, remainder);
-	else if (remainder < 6 && remainder != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, remainder);
+	if ((n % 10) > 5)
+		printf("Last digit of %d is %d  and is greater than 5", n, n % 10);
+	else if ((n % 10) < 6 && (n % 10) != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, n % 10);
 	else
-		printf("Last digit of %d is 0", n);
+		printf("Last digit of %d is 0", n, n % 10);
 	return (0);
 }
