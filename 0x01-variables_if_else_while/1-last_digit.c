da3d5f297155a8d4fd27c068540d48a772ebcc20
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-08 17:59:56 +0200

    added missing semi-colon

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,13 +12,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	remainder = n % 10
+	remainder = n % 10;
 
 	if (remainder > 5)
 		printf("Last digit of %d is %d  and is greater than 5", n, remainder);
 	else if (remainder < 6 && remainder != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, remainder);
 	else
-		printf("Last digit of %d is 0");
+		printf("Last digit of %d is 0", n);
 	return (0);
 }
