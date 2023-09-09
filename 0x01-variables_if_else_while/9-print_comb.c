9c08e7105ffe40b2b94b78f43690be9b35196312
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-09 12:04:18 +0200

    checking 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+	* main - Entry point
+	* Description: Uses putchar to print a string
+	* Return: Always 0 (Success)
+	*/
+int main(void)
+{
+	int array;
+
+	for (array = 0; 0 <= 9; array++)
+		putchar('0' + array)
+		putchar(', ')
+	putchar('\n')
+}
