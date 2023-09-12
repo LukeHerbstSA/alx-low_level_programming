609e06eb4a0264ea7373a5199a07a6e94e4f2504
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-12 15:52:28 +0200

    test

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-/**
-	* print_alphabet - prototype function
-	* Description: prints the alphabet in lowercase
-	* Return: Always 0 (Success)
-	*/
+#include "print_alphabet.h"
 /**
 	* main - Entry point
 	* Description: calls print_alphabet
@@ -16,6 +12,10 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+/**
+	* print_alphabet - prototype function
+	* Description: prints the alphabet
+	* Return: Always 0 (Success)
 void print_alphabet(void)
 {
 	int i;
