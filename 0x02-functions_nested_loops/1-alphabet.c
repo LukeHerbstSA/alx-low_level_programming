dc2449891877fa903b90a8bec1a517d87a02b6b3
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-12 23:05:55 +0200

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 #include "main.h"
 /**
-	* main - Entry point
-	* Description: calls print_alphabet
-	* Return: Always 0 (Success)
-	*/
-void print_alphabet(void);
-int main(void)
-{
-	print_alphabet();
-	putchar('\n');
-	return (0);
-}
-/**
 	* print_alphabet - prototype function
 	* Description: prints the alphabet
 	* Return: Always 0 (Success)
 	*/
-
 void print_alphabet(void)
 {
 	int i;
 
 	for (i = 'a'; i < 'z'; i++)
 		putchar(i);
+}
+/**
+	* main - Entry point
+	* Description: calls print_alphabet
+	* Return: Always 0 (Success)
+	*/
+int main(void)
+	print_alphabet();
+	putchar('\n');
+	return (0);
 }
