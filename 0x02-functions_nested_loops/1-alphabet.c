f7b36c976d7a1518b60aacd82dde6041a516c26f
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-13 23:21:11 +0200

    test betty style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
-        * print_alphabet - void function
-        * Description: prints the alphabet
-        * Return: void
-        */
+	* print_alphabet - void function
+	* Description: prints the alphabet
+	* Return: void
+	*/
 void print_alphabet(void)
-{       
-        int i;
-        
-        for (i = 'a'; i <= 'z'; i++)
-                putchar(i);
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
 	putchar('\n');
 }
