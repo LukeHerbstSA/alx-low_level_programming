e2c117040212926716d9ded85481919fb927bc12
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-13 23:18:46 +0200

    test

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #ifndef FUNCTIONS_HEADER_H
 #define FUNCTIONS_HEADER_H
-void print_alphabet(void)
-void print_alphabet_x10(void)
-int islower(int c)
-int isalpha(int c)
-int print_sign(int c)
-int add(int a, int b)
-void print_to_98(int n)
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int islower(int c);
+int isalpha(int c);
+int print_sign(int c);
+int add(int a, int b);
+void print_to_98(int n);
 #endif 
