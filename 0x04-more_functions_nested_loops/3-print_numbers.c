9988b36577237026fbe8221f39e4d8d3c0457ef7
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-14 22:02:24 +0200

    fixed changing putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,12 @@
 /**
 	* print_numbers - void function
 	* Description: prints numbers from 0-9
-	* Return: none
 	*/
-
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		putchar(i);
+		putchar('0' + i);
 	putchar('\n');
 }
