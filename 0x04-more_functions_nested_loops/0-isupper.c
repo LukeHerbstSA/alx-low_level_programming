e11b6d05a88db2b248ff5085aca92ee626c751f9
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-14 15:04:40 +0200

    test

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 	* _isupper - int function
@@ -11,6 +12,6 @@ int _isupper(int c);
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-
-	return (0);
+	else
+		return (0);
 }
