000cb029afe9b07f38cf0e59916ff903fe2e5a68
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-14 13:40:17 +0200

    test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+	* main - Entry point
+	* Description: This program tests the output of a called function
+	* Return: Always 0 (Success)
+	*/
+int main(void)
+{
+	int num;
+
+	num = 0;
+	positive_or_negative(num);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This file concerns debugging code only
