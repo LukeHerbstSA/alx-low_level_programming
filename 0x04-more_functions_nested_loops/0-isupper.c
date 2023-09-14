22d3cfa78f4492193d16ada157d4d083f31feb7f
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-14 14:57:11 +0200

    test

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+	* _isupper - int function
+	* Description:
+	* @c: int type
+	* Return: 1 if true, 0 if not
+	*/
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	
+	return (0);
+}
