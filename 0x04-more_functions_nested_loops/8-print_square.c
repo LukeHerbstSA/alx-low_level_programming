95a11243ec19f4a0e837b1f1782c0da9a52c2493
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-15 13:40:06 +0200

    fixed minor syntax mistakes

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,13 +10,13 @@ void print_square(int size)
 	int i;
 	int j;
 
-	if (size <= 0) 
+	if (size <= 0)
 		putchar('\n');
 	else
 	{
 		for (i = 0; i < size; i++)
 		{
-			for(j = 0; j < size; j++)
+			for (j = 0; j < size; j++)
 				putchar('#');
 			putchar('\n');
 		}
