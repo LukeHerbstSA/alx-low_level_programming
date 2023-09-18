c0fb4ab7f2cef6a6df940a28911da85261afd6ac
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-18 21:54:03 +0200

    finshed, used 2 counters at once

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h'
+
+/**
+	* rev_string - void function
+	* Description: reverses a string
+	* @s: passed pointer to array
+	*/
+void rev_string(char *s);
+{
+	int i;
+	float length;
+	char nrml;
+	char revrs;
+
+	j = -1;
+	length = strlen(s)/2;
+	for (i = 0; i < length; i++)
+	{
+		nrml = s[i];
+		revrs = s[j];
+
+		s[i] = revrs;
+		s[j] = nrml;
+		j = j - 1;
+	}
+	putchar('\n');
+}
