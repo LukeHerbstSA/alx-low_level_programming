74c190eb4aedf45e962c7c6db169914ebdf54a9f
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-18 15:10:03 +0200

    completed several

---
+++ 0x05-pointers_arrays-strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+	* swap_int - takes 2 pointers
+	* Description: 
+	* @a: int pointer
+	* @b: int pointer
+	*/
+void swap_int(int *a, int *b);
+{
+	num1 = *a;
+	num2 = *b;
+	*a = num2;	
+	*b = num1;
+	return (0);
+}
---
+++ 0x05-pointers_arrays-strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+	* _puts - void function
+	* Description: This function prints a string by accessing an array
+	* @str: pointer to string
+	*/
+void _puts(char *str);
+{
+	int i;
+	length = strlen(*str);
+
+	for (i = 0; i < length - 1; i++)
+		putchar((*str)[i]);
+	return (0);
+}
