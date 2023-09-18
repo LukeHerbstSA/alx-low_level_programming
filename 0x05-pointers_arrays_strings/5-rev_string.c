243a064a34c34f6d59397a410c8e4577c34b1055
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-19 00:18:35 +0200

    test

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,19 +7,5 @@
 	*/
 void rev_string(char *s)
 {
-	int i;
-	int j;
-	int length;
-	char nrml;
-
-	j = -1;
-	length = strlen(s) - 1;
-	for (i = 0; i < length / 2; *s++)
-	{
-		nrml = *s;
-		*s = *s[j];
-		*s[j] = nrml;
-		j--;
-		
-	}
+	strrev(s);
 }
