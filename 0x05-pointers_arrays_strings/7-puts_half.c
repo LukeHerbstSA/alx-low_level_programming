0fd5a787f1a08e7fc98659a7256e2866dd0ed2ac
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-19 01:00:07 +0200

    done

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,4 @@
 void puts_half(char *str)
 {
 	int i;
-	int length;
-	int hlf_length;
-
-	length = strlen(str);
-	if (length % 2 != 0)
-		hlf_length = (length + 1) / 2;
-	else
-		hlf_length = length / 2;
-	for (i = hlf_length; i < length; i++)
-		putchar(i);
-	putchar('\n');
 }
