b2446c2b24c21b97aa0e861e60d02afa7d14dd57
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-21 21:57:24 +0200

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,21 +16,24 @@ char *cap_string(char *ex)
 	start = 0;
 	for (i = 0; ex[i] != '0\'; i++)
 	{
+		for (j = 0; regex[j] != '\0'; j++)
+		{
+			if (ex[i] == regex[j]);
+				start = 1;
+		}
 		if (start == 1)
 		{
-			if ((ex[i] >= 97) && (ex[i] <= 122))
+			if (ex[(i + 1)] >= 97 && ex[(i + 1)] <= 122)
 			{
-				ex[i] = ex[i] - 32;
+				ex[(i + 1)] = ex[(i + 1)] - 32;
 				start = 0;
 			}
 		}
-		for (j = 0; regex[j] != '\0'; j++)
-		{
-			if (regex[j] == ex[i])
-				start = 1;
-		}
 		if (i = 0)
-			ex[i] = ex[i] - 32;
+		{
+			if (ex[i] >= 97 && ex[i] <= 122)
+				ex[i] = ex[i] - 32;
+		}
 	}
 	return (ex);
 }
