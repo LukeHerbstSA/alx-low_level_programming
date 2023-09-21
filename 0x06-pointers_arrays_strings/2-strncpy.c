4da84f66559500fac781b1d17505b334fe6de7e2
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-21 22:18:33 +0200

    fixed #2strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,22 +11,17 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int x;
 	int j;
 
-	i = 0;
-	x = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[x] != '\0')
-		x++;
-	for (j = 0; j < n; j++)
+	j = 0;
+	while (src[j] != '\0')
+		j++;
+	for (i = 0; i < n; i++)
 	{
-		if (j >= x)
+		if (i >= j)
 			dest[i] = '\0';
 		else
-			dest[i] = src[j];
-		i++;
+			dest[i] = src[i];
 	}
 	return (dest);
 }
