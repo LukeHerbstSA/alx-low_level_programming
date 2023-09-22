92c890c110991aa671dd5a7eb2cd35715f201d63
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-22 16:52:31 +0200

    added solution for 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+	* _strpbrk - char function
+	* Description: returns pointer to any match in accept
+	* @s: passed string
+	* @accept: instance holder
+	* Return: returns pointer s
+	*/
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*s == *accept)
+			{
+				return (s);
+			}
+			accept++;
+		}
+		s++;
+	}
+	return (NULL);
+}
