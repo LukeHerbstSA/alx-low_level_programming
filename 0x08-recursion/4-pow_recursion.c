a11a5d05179b1283e27a99731217cee18031252c
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-27 20:54:13 +0200

    fixed #4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,16 +9,13 @@
 	*/
 int _pow_recursion(int x, int y)
 {
-	if (y > 1)
-	{
-		x = x * x;
-		y--;
-		return (_pow_recursion(x, y));
-	}
 	if (y == 1)
 		return (x);
 	if (y == 0)
 		return (1);
 	if (y < 0)
 		return (-1);
+	x = x * x;
+	y = y - 1;
+	return (_pow_recursion(x, y));
 }
