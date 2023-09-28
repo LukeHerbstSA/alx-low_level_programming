04f0c9c0139a43b4df362d11ca23513336d7414f
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-28 17:54:26 +0200

    done

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+	* _sqrt_recursion - int function
+	* Description: Returns the sqrt of a number
+	* @n: passed square
+	* Return: -1 if n <= 0 or the root
+	*/
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	if (n <= 0)
+		return (-1);
+	i = 0.5 * n;
+	if (i * i == n)
+		return (i);
+	return (_sqrt_recursion(i));
+}
