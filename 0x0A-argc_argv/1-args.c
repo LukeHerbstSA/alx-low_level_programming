0d8f08191e15a8b12165b690f4f05a9f46ea1852
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-29 19:15:27 +0200

    added solution for 1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+	* main - int function
+	* Description: prints the num of elements in argv
+	* @argv: passed array of program args
+	* Return: Always 0 (Success)
+	*/
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
