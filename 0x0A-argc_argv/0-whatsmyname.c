e5fccc9d257d222fe1ee2a231b43cd6dc821506d
System Administrator <root@Lukes-MacBook-Air.local>
2023-09-29 19:07:42 +0200

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-	* _my_name - int function
-	* Description: prints its own file name
-	* @argc: number of elements in argv
-	* @argv: pointer to array of arguments passed to prog
-	* Return: 0 (Success)
+	* main - Prints its own file name
+	* @argc: num of elements in argv
+	* @argv: pointer to an array of the supplied string args
+	* Return: Always 0 (Success)
 	*/
-int _my_name(int __attribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
