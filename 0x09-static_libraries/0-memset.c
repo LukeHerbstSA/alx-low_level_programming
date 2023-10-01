69168982f66fd6d31204cab4816e78fb2f7f2255
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-01 21:06:47 +0200

    removed libray, going to re add on ubuntu containter

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,12 @@
+/**
+	* _memset - char function
+	* Description: test
+	* @s: passed array
+	* @b: character
+	* @n: usigned n
+	* Return: int
+	*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
