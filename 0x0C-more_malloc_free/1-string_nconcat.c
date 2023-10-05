8d92d4d599728e16f3f7208cbbd110af35617cb2
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-05 16:01:39 +0200

    fixing memory leak

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,22 +11,26 @@
 	*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
-	unsigned int j;
+	int i;
+	int j;
 	char *ptr;
 
+	i = 0;
+	j = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n > sizeof(s2))
-		n  = sizeof(s2);
-	ptr = malloc(sizeof(s1) + n + 1);
+	while (s1[i] != '\0')
+		i++;
+	while (j < n && s2[j] != '\0')
+		j++;
+	ptr = malloc(i + j + 1);
 	if (ptr != NULL)
 	{
-		for (i = 0; i < sizeof(s1); i++)
+		for (i = 0; s1[i] != '\0'; i++)
 			ptr[i] = s1[i];
-		for (j = 0; j < n; j++)
+		for (j = 0; j < n && s2[j] != '\0'; j++)
 		{
 			ptr[i] = s2[j];
 			i++;
