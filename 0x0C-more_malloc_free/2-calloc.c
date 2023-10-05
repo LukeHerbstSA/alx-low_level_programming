de1a0bfe3923445fa1158816b1120025e809e7cf
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-05 18:25:51 +0200

    added solution for 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+	* _calloc - void function
+	* Description: allocates memory for an array
+	* @nmemb: number of elements
+	* @size: size of each element
+	* Return: void ptr
+	*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	int i;
+	int bytes;
+
+	bytes = nmemb * size;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(size);
+	if (ptr != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			*((char *)ptr) = 0;
+			ptr++;
+		}
+	}
+	else
+		return (NULL);
+	return (ptr);
+}
