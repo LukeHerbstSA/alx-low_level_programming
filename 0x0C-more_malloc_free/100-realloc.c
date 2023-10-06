1a805f53d253e23c151b7e20742aa2ff4a82a303
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-06 22:25:57 +0200

    added solutiong for 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,12 +15,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	else if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else if (ptr == NULL)
+	if (ptr == NULL)
 	{
 		cpptr = malloc(new_size);
 		return (cpptr);
