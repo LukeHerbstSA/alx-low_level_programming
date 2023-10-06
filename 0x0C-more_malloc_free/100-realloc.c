63535f1f3ec96eeb79e4625813cc8652542f9d5a
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-06 15:17:25 +0200

    added solutiong for 100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+	* _realloc - void function
+	* @ptr: passed allocated memory address
+	* @old_size: old size of ptr
+	* @new_size: the amount of memory to be reassigned to ptr
+	* Return: pointer if successful, or NULL
+	*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *cpptr;
+	int i;
+
+	i = 0;
+	if (new_size == old_size)
+		return (ptr);
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else
+		cpptr = malloc(new_size);
+		if (cpptr != NULL)
+			for (i = 0; ((char *)(ptr))[i] != '\0'; i++)
+				((char *)(cpptr))[i] = ((char *)(ptr))[i];
+		else
+			return (NULL);
+	return (ptr);
+}
