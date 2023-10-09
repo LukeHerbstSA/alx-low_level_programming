f3992680165756f606b1c09a00b8aec7e945afef
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-09 20:21:56 +0200

    added several solutions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,18 +8,11 @@
 	*/
 void print_dog(struct dog *d)
 {
-	char *error[5] = "(nil)";
-
-	if (d[0] == NULL)
-		printf("Name: %s\n", error);
-	else
-		printf("Name: %s\n", d[0]);
-	if (d[1] == NULL)
-		printf("Age: %s\n", error);
-	else
-		printf("Age: %f\n", d[1]);
-	if (d[2] == NULL)
-		printf("Owner: %s", error);
-	else
-		printf("Owner: %s", d[2]);
+	if ((*d).name == NULL)
+		(*d).name = "(nil)";
+	if ((*d).age == NULL)
+		(*d).age = "(nil)";
+	if ((*d).owner == NULL)
+		(*d).owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s", (*d).name, (*d).age, (*d).owner);
 }
