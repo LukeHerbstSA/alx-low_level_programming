dbdd96b8512072e2ecd32181df1e5a19f68b32ed
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-09 15:14:38 +0200

    added solution for 2 - used __FILE__ macro to print path of file at compilation time - also added success return

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-void main()
+/**
+	* main - void func
+	* Description: prints path of file at compilation time
+	* Return: Always 0
+	*/
+int main(void)
 {
 	printf("%s\n", __FILE__);
+
+	return (0);
 }
