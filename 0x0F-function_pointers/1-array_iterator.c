8f321d9777f385135400ac38b395583e63bbb3be
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-11 15:36:11 +0200

    added soltion for 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+	* array_iterator - void function
+	* Description: calls action ptr for every element of array
+	* @array: pointer to array of ints
+	* @size: num of elements in array
+	* @action: func pointer
+	*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
+	}
+	else
+		return;
+}
