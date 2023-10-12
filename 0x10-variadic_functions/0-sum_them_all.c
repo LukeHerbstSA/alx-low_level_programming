b342613f7101c13583d69f9e8077b94ec43ebae4
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-12 11:54:10 +0200

    added 0 return if args = 0 and changed i to an unsigned int in order to allow comparison with n

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,15 +12,15 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list adder;
 	int total;
-	int i;
+	unsigned int i;
 
 	va_start(adder, n);
 
 	total = 0;
+	if (n == 0)
+		return (0);
 	for (i = 0; i < n; i++)
-	{
 		total = total + va_arg(adder, int);
-	}
 	va_end(adder);
 	return (total);
 }
