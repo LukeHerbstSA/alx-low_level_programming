dcd51b4221e750dbff6b716e681feee6077f4396
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-13 14:49:01 +0200

    added solution for 3 - using switch case

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+	* print_all - void function
+	* Description: prints every argument passed
+	* @format: list of types passed
+	*/
+void print_all(const char * const format, ...)
+{
+	va_list printer;
+	int i;
+	int elements;
+
+	va_start(printer, format);
+	while (i < sizeof(format) - 1 && format != NULL)
+	{
+		switch (format[i])
+		{
+			case 'c':
+			{
+				printf("%c", va_arg(printer, int));
+				break;
+			}
+			case 'i':
+			{
+				printf("%d", va_arg(printer, int));
+				break;
+			}
+			case 'f':
+			{
+				printf("%f", va_arg(printer, double));
+				break;
+			}
+			case 's':
+			{
+				if (va_arg(printer, char *) != NULL)
+					printf("%s", va_arg(printer, char *));
+				else
+					printf("(nil)");
+				break;
+			}
+		}
+		if (format[i] != elements - 1)
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
+	va_end(printer);
+}
