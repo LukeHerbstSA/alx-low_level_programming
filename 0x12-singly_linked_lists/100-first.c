2c3455d9d2137f031f98ef31877ef7d4f557066b
Luke Herbst <lukeherbst37@gmail.com>
2023-10-19 20:55:55 +0200

    added solution for 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+inline void printer();
+
+/**
+	* main - void
+	* Description: calls printer
+	*/
+void main()
+{
+	printer();
+}
+/**
+	* printer - inline func
+	* Description: prints set string faster than main
+	*/
+inline void printer()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
