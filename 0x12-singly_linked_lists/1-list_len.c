cc7ae32c5981856dc39878c6cc2862bf881714d3
Luke Herbst <lukeherbst37@gmail.com>
2023-10-19 14:45:45 +0200

    added solution to 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+	* list_len - size_t return
+	* Description: counts number of elements in struct ptr
+	* @h: passed pointer to struct
+	* Return: size of singly list
+	*/
+size_t list_len(const list_t *h)
+{
+	size_t elements;
+	for (elements = 0; h != NULL; elements++)
+		h = h->next;
+	elements = elements * 3;
+	return (elements);
+}
