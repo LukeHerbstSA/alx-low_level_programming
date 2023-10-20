bbf2448fa30aba48ca824e3d09f801f51da96170
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-20 18:27:31 +0200

    testing checks

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,15 @@
 section .text
 	global _start
+	extern printf
 
 _start:
 	mov edx, len
 	mov ecx, holberton
 	mov ebx, 1
 	mov eax, 4
-	int 0x80
 
 	mov eax, 0
-	int 0x80
+	ret
 ; Damn why name storages funky names like edx, ecx etc?
 ; why not just give user friendly names like edx = int reg?
 ; or exc = char reg?
