8b53fc84cb75b9db3e9f0c53c992628c6ce61529
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-20 18:10:21 +0200

    added solution for 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,3 +1,18 @@
-Section .data
-	greeting:	db "Hello, Holberton!\n"
-	len:		
+section .text
+	global _start
+
+_start:
+	mov edx, len
+	mov ecx, holberton
+	mov ebx, 1
+	mov eax, 4
+	int 0x80
+
+	mov eax, 1
+	int 0x80
+; Damn why name storages funky names like edx, ecx etc?
+; why not just give user friendly names like edx = int reg?
+; or exc = char reg?
+section .data
+	holberton db "Hello, Holberton",10 ; 10 = ascii for \n
+	len equ $ -holberton
