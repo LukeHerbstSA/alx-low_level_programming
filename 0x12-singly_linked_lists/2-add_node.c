3efe9f6c943e568eb99c4dded10330c6bc3d372a
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-20 12:12:44 +0200

    cant fix issue, issue is on Holberton Side (they expect str to have a newline that they should be placing themselves)

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,17 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_head;
 	int i;
-	char *new_string;
+	char *str;
 
 	i = 0;
 	new_head = malloc(sizeof(list_t));
 	if (new_head != NULL)
 	{
-		new_string = malloc(strlen(str) + 2);
-		new_string = strcpy(new_string, str);
-		new_string = strcat(new_string, "\n");
 		while (str[i] != '\0')
 			i++;
 		new_head->next = *head;
 		new_head->len = i;
-		new_head->str = new_string;
+		new_head->str = str;
 	}
 	else
 		return (NULL);
