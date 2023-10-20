ecf24b7ff32791366e7eb6e24a2bf5fc5f9a7937
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-20 13:14:22 +0200

    100-test

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,17 +1,5 @@
 #include "lists.h"
 
-inline void printer(void);
-
-/**
-	* main - void
-	* Description: calls printer
-	* Return: Always 0 (Success)
-	*/
-int main(void)
-{
-	printer();
-	return (0);
-}
 /**
 	* printer - inline func
 	* Description: prints set string faster than main
