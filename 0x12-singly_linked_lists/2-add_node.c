8c756bff3beacf15bf0bb43b62d6afca71a05e1c
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-20 11:57:33 +0200

    used strcpy and strcat to make a duplicate of passed string with newline to satisfy output demands

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,23 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_head;
 	int i;
+	char *new_string;
 
 	i = 0;
 	new_head = malloc(sizeof(list_t));
 	if (new_head != NULL)
 	{
+		new_string = malloc(strlen(str) + 2);
+		new_string = strcpy(new_string, str);
+		new_string = strcat(new_string, "\n");
 		while (str[i] != '\0')
 			i++;
 		new_head->next = *head;
 		new_head->len = i;
-		new_head->str = strdup(str);
+		new_head->str = new_string;
 	}
 	else
 		return (NULL);
 	*head = new_head;
-	printf("\n");
 	return (*head);
 }
