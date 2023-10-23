23ead8872ff5636e9edaf134705d0e811eca5757
Luke Herbst <lukeherbst37@gmail.com>
2023-10-23 19:44:07 +0200

    solution to 4

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+	* add_nodeint_end - listint_t func
+	* Description: adds a node to the end
+	* @head: ptr to ptr that holds address of 1st node
+	* @n: value to be stored in last node
+	* Return: listint_t address to start of last element
+	*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	int i;
+	listint_t *last;
+	listint_t *tmp;
+
+	last = malloc(sizeof(struct listint_s));
+	if (last != NULL)
+	{
+		last->n = n;
+		last->next = NULL;
+		tmp = *head;
+		if (*head != NULL)
+		{
+			for (i = 0; tmp != NULL; i++)
+			{
+				if (tmp->next == NULL)
+					tmp->next = last;
+					break;
+				tmp = tmp->next;
+			}
+		}
+		else
+			tmp = last;
+	}
+	else
+		return (NULL);
+	return (last);
+}
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/*
+	* free_listint - void func
+	* Description: frees passed singly
+	* @head: pointer to start of singly
+	*/
+void free_listint(listint_t *head)
+{
+	int i;
+	listint_t *tmp;
+
+	if (head != NULL)
+	{
+		for (i = 0; head != NULL; i++)
+		{
+			tmp = head;
+			head = head->next;
+			free(tmp);
+		}
+	}
+	else
+		return;
+}
