cb773fce58059830b04e29af687cee1029deb301
Luke Herbst <lukeherbst37@gmail.com>
2023-10-23 18:19:45 +0200

    added solution for 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+	* add_nodeint - listint_t func
+	* Description: adds a new odd node with remapping
+	* @head: ptr to ptr that holds address of previous head
+	* @n: value to be store in new_head n (int val)
+	* Return: address of new element (head ptr) or NULL - fail
+	*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_head;
+	new_head = malloc(sizeof(struct listint_s));
+	if (new_head != NULL)
+	{
+		new_head->n = n;
+		new_head->next = *head;
+	}
+	else
+		return (NULL);
+	return (new_head);
+}
