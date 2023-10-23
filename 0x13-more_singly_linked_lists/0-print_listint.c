3abeccdc3372a2564bc4dc3bac51178b4416f0d0
Luke Herbst <lukeherbst37@gmail.com>
2023-10-23 17:06:32 +0200

    added solution to 0 - return 0 on null pointer

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+	* print_listint - size_t func
+	* Description: prints all elements of a linked list
+	* @h: passed pointer to linked list (head node)
+	* Return: int of type (size_t)
+	*/
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	if (h != NULL)
+		for (i = 0; h != NULL; i++)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
+	else
+		return (0);
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory is made up of more advanced singly questions
