e3ef87db98cca675b6a53f27477ab23ae04601da
Luke Herbst <lukeherbst37@gmail.com>
2023-10-24 20:55:24 +0200

    added solution to 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+	* get_nodeint_at_index - listint_t func 
+	* Description: returns node at the given passed index
+	* @head: passed linked lists
+	* @index: position to return
+	* Return: address to node
+	*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	i = 0;
+	while (head != NULL)
+	{
+		if (i == index)
+			return (head);
+		i++;
+		head = head->next;
+	}
+	return (NULL);
+}
