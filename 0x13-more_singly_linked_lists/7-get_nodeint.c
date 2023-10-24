ca68242baa6bb093468ba60b729dc05001d47dde
Luke Herbst <lukeherbst37@gmail.com>
2023-10-24 21:23:30 +0200

    check

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,18 +12,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 
 	i = 0;
-	if (index = 0)
-		return (head);
-	if (head != NULL)
+	while (head != NULL)
 	{
-		while (head != NULL)
-		{
-			if (i = index)
-				return (head);
-			i++;
-			head = head->next;
-		}
+		if (i == index)
+			return (head);
+		i++;
+		head = head->next;
 	}
-	else
-		return (NULL);
+	return (NULL);
 }
