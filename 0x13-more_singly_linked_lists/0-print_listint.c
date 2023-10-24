fc98db9155ac0944c9e3da862b8218410c6791fd
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-24 15:19:02 +0200

    test

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,15 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i;
 
+	i = 0;
 	if (h != NULL)
 	{
-		for (i = 0; h != NULL; i++)
+		while (h != NULL)
 		{
 			printf("%d\n", h->n);
 			h = h->next;
+			i++;
 		}
 	}
-	else
-		return (0);
 	return (i);
 }
