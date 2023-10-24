4ac3e77cb17eacca16683081eec83d71b88ecd26
Luke Herbst <lukeherbst37@gmail.com>
2023-10-24 16:42:27 +0200

    added solution to 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+	* free_listint2 - void function
+	* Description: frees passed list and sets head to null
+	* @head: passed ptr to ptr to start of linked list
+	*/
+void free_listint2(listint_t **head)
+{
+	listint_t *freedom;
+
+	if (*head != NULL)
+	{
+		while (*head != NULL)
+		{
+			freedom = *head;
+			*head = (*head)->next;
+			free(freedom);
+		}
+	}
+	head = NULL;
+	return;
+}
