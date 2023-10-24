ab9fb9ce42390a0ac7e4dd9a33f193c6c8d349ec
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-24 16:12:02 +0200

    in process of rewriting

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,5 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i;
 
-	i = 0;
-	while (h->next != NULL)
-	{
-		printf("%d\n", h->n);
-		i++;
-		h = h->next;
-	}
-	printf("%d\n", h->n);
-	return (i + 1);
+	
 }
