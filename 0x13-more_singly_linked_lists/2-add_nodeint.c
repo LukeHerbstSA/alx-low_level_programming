10d5d2e7e161690d7630ce54eed4726b6421a734
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-24 13:32:51 +0200

    fixed betty issue

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
 	* add_nodeint - listint_t func
-	* Description: adds a new odd node with remapping
+	* Description: adds a new head node with remapping
 	* @head: ptr to ptr that holds address of previous head
 	* @n: value to be store in new_head n (int val)
 	* Return: address of new element (head ptr) or NULL - fail
@@ -12,6 +12,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_head;
 	new_head = malloc(sizeof(struct listint_s));
+
 	if (new_head != NULL)
 	{
 		new_head->n = n;
