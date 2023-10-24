6534822c58f3eafac5b8a066ad91d8cd48dc6ba3
Luke Herbst <lukeherbst37@gmail.com>
2023-10-24 20:58:08 +0200

    tried different layout for checker

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,10 +14,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	i = 0;
 	while (head != NULL)
 	{
-		if (i == index)
+		if (i != index)
+			i++;
+			head = head->next;
+		else
 			return (head);
-		i++;
-		head = head->next;
 	}
 	return (NULL);
 }
