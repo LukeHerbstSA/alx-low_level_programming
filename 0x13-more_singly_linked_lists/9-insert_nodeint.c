9eaa11ba49ac8da4bf2e9dcc5bb0750876e63a2e
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-25 13:15:10 +0200

    fixed betty errors for 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	i = 0;
 	if (new_node != NULL)
 	{
-		if (idx = 0)
+		if (idx == 0)
 			*head = new_node;
 		else
 		{
@@ -36,13 +36,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				}
 			}
 		}
-		if (i != idx)
-			return (NULL);
-		else
+		if (i == idx)
 		{
 			new_node->n = n;
 			new_node->next = tmp;
 		}
+		else
+			return (NULL);
 	}
 	else
 		return (NULL);
