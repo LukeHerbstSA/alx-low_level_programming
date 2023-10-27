786ec117db5a3422f45aaa54b8909e9f6ce8a0f9
Luke Herbst <lukeherbst37@gmail.com>
2023-10-27 15:26:15 +0200

    added solution for 1 - betty testing due now

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+	* print_binary - void func
+	* Description: prints the binary rep of a number
+	* @n: binary number
+	*/
+void print_binary(unsigned long int n)
+{
+	int i;
+
+	for (i = 63; i >= 0; i--)
+	{
+		if (((n << i) & 1) == 1)
+			break;
+	}
+	if (i != 0)
+	{
+		while (i >= 0)
+		{
+			putchar((n << i) & 1);
+			i--;
+		}
+	}
+	else
+		putchar('0');
+}
