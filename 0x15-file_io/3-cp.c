b3eee81fe82323ecd6fb7a2c3fdc62b3cf53d58f
System Administrator <root@Lukes-MacBook-Air.local>
2023-10-31 18:07:30 +0200

    added solution to 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,32 +10,65 @@
 unsigned int cp(int argc, char *argv[])
 {
 	char text[1024];
-	int close_check;
+	int gen_check;
 	int file_from;
 	int file_to;
 
-	if (argc != 2)
+	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n")
-		exit(97);
+		error_handler(-1, -1, NULL);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file NAME_OF_THE_FILE\n");
-		exit(98);
-	}
+		error_handler(0, -1, argv[1]);
 	if (file_to == -1)
+		error_handler(1, -1, argv[2]);
+	while (gen_check == 1024)
 	{
-		if (close(file_from) == -1)
-		{
-			dprintf(2, "Error: Can't close fd %d\n", file_from);
-			exit(100);
-		}
-		close(file_from);
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		gen_check = read(file_from, text, 1024);
+		if (gen_check == -1)
+			error_handler(0, -1, argv[1]);
+		gen_check = write(file_to, text, gen_check);
+		if (gen_check == -1)
+			error_handler(1, -1, argv[2]);
+	}
+	gen_check = close(file_from);
+	if (gen_check == -1)
+		error_handler(2, file_from, NULL);
+	gen_check = close(file_to);
+	if (gen_check == -1)
+		error_handler(2, file_to, NULL);
+	return (0);
+}
+
+/**
+	* error_handler - void func
+	* Description: exits with appropriate exit code via issue_type num
+	* @issue_type: error number
+	* @file_descriptor: not always relevant, refers to stream number
+	* @filename: not always relevant, holds associated filename
+	*/
+void error_handler(int issue_type, int file_descriptor, char *filename)
+{
+	if (issue_type == 2)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_descriptor);
+		exit(100);
+	}
+	if (issue_type == 1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(99);
 	}
-	
+	if (issue_type == 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	if (issue_type == -1)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+unsigned int cp(int argc, char *argv[]);
+void error_handler(int issue_type, int file_descriptor, char *filename);
 
 #endif
