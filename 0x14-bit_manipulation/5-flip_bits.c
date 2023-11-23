0c617b6ec37037cc0430d28ce5086dda20620d58
Luke Herbst <lukeherbst37@gmail.com>
2023-11-23 16:51:59 +0200

    added solution for flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+	* flip_bits - unsigned int func
+	* Description: returns the number of bits to get from n to m
+	* @n: number 1
+	* @m: number 2
+	* Return: number of bits flipped
+	*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bit1 = 0;
+	unsigned int bit2 = 0;
+	unsigned long int flips = 0;
+	int i;
+
+	for (i = 0; i <= 31; i++)
+	{
+		bit1 = n & (1 << i);
+		bit2 = n & (1 << i);
+		if (bit1 != bit2)
+			flips++;
+	}
+	return flips;
+}
