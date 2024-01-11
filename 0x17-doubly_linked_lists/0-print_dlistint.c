ff4f2470885425a30694c48b7c12fa4ae6a3b338
root <lukeherbst37@gmail.com>
2024-01-11 03:20:37 -0800

    rewrites

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,14 @@
 	*/
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *tmp = *h;
+	dlistint_t *tmp = h;
 	size_t i = 0;
 
 	while (tmp != NULL)
 	{
-		printf("%zu\n", tmp->n);
+		print("%d\n", tmp->n);
 		tmp = tmp->next;
 		i++;
 	}
-	return (i)
+	return (i);
 }
