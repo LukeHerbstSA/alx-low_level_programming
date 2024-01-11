78b75b6e6b891dc75597949717fae6605dd4371c
root <lukeherbst37@gmail.com>
2024-01-11 03:49:29 -0800

    added function description

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+	* dlistint_len - size_t func
+	* Description: returns number of elements
+	* @h: pointer to doubly list
+	* Return: size_t nodes
+	*/
 size_t dlistint_len(const dlistint_t *h)
 {
 	dlistint_t *tmp = h;
