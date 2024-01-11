934c2b3290d9316b756fba1f1d6fa755d8b8c285
root <lukeherbst37@gmail.com>
2024-01-11 02:48:00 -0800

    first solution

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+	* print_dlistint - size_t func
+	* Description: prints all elements of singly
+	* @h: pointer to doubly linked list
+	* Return: returns number of elements
+	*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t *tmp = *h;
+	size_t i = 0;
+
+	while (tmp != NULL)
+	{
+		printf("%zu\n", tmp->n);
+		tmp = tmp->next;
+		i++;
+	}
+	return (i)
+}
