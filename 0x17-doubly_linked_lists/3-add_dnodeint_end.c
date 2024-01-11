04f123b5c33b6486be60a9b6e6d1161be380a91f
root <lukeherbst37@gmail.com>
2024-01-11 06:13:07 -0800

    more syntax fixes

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,16 +10,18 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *tmp = *head;
-	dlistint_t newnode = malloc(sizeof(dlistint_t));
+	dlistint_t *newnode = malloc(sizeof(dlistint_t));
 
 	if (newnode ==  NULL)
 		return (NULL);
 	if (tmp == NULL)
+	{
 		*head = newnode;
 		(*head)->next = NULL;
 		(*head)->prev = NULL;
 		(*head)->n = n;
 		return (*head);
+	}
 	while (tmp->next != NULL)
 	{
 		tmp = tmp->next;
--- 0x17-doubly_linked_lists/testscript.c
+++ 0x17-doubly_linked_lists/testscript.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-#include "2-add_dnodeint.c"
+#include "3-add_dnodeint_end.c"
 #include "0-print_dlistint.c"
 
 /**
@@ -15,14 +15,14 @@ int main(void)
 	dlistint_t *head;
 
 	head = NULL;
-	add_dnodeint(&head, 0);
-	add_dnodeint(&head, 1);
-	add_dnodeint(&head, 2);
-	add_dnodeint(&head, 3);
-	add_dnodeint(&head, 4);
-	add_dnodeint(&head, 98);
-	add_dnodeint(&head, 402);
-	add_dnodeint(&head, 1024);
+	add_dnodeint_end(&head, 0);
+	add_dnodeint_end(&head, 1);
+	add_dnodeint_end(&head, 2);
+	add_dnodeint_end(&head, 3);
+	add_dnodeint_end(&head, 4);
+	add_dnodeint_end(&head, 98);
+	add_dnodeint_end(&head, 402);
+	add_dnodeint_end(&head, 1024);
 	print_dlistint(head);
 	return (EXIT_SUCCESS);
 }
